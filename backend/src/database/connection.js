41da6561f9cff78ce1c7c968593c130353894d29
AdelRizq <adel.rizq99@eng-st.cu.edu.eg>
2021-12-23 20:54:43 +0200

    refactor(File Structure): :fire: remove backend folder

--- backend/src/database/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize('lms', 'root', 'Mahboub123', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
-global.sequelize = sequelize;
