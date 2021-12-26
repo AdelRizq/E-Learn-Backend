1e037a8aeba48993419a2826cc4b18b3e2c587a5
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2021-12-27 01:00:17 +0200

    feat(auth): :sparkles:

---
+++ src/auth/auth.js
@@ -0,0 +1,21 @@
+const httpStatus = require("http-status")
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization']
+
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+
+        req.token = bearerToken;
+        next();
+    } else {
+        res.status(httpStatus.FORBIDDEN).send({
+            message: "This operation is forbibden for you hehe hahhaha !"
+        });
+    }
+}
+
+module.exports = {
+    verifyToken
+}
