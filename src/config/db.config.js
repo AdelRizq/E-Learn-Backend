f5ea1d34d937b23f0c844aee19f68bae743afef3
AdelRizq <adel.rizq99@eng-st.cu.edu.eg>
2021-12-27 12:49:28 +0200

    build(Database): :wrench: Edit db configuration file

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,14 +1,29 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'root',
-    DB: 'lms',
-    dialect: 'mysql',
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "root",
+    DB: "lms",
+    dialect: "mysql",
 
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
-        idle: 10000
-    }
+        idle: 10000,
+    },
 };
+
+// module.exports = {
+//     HOST: "sql11.freemysqlhosting.net",
+//     USER: "sql11461123",
+//     PASSWORD: "HuuCN9953R",
+//     DB: "sql11461123",
+//     dialect: "mysql",
+
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// };
